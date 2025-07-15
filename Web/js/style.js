737a74dbdef2ad09fc15b0faa85059c2add0f176
Manley.B <manley-bouille@outlook.fr>
2025-07-16 10:01:02 +1100

    modified:   Web/css/style.css 	modified:   Web/index.html 	modified:   Web/js/style.js

--- Web/js/style.js
+++ Web/js/style.js
@@ -1,16 +1,11 @@
 const explicationBox = document.getElementById('explication');
 
 function changeTextWithFade(newText) {
-    // Étape 1 : on fade-out
     explicationBox.classList.add('fade-out');
 
-    // Étape 2 : après le fade-out (400ms), on change le texte, puis fade-in
     setTimeout(() => {
         explicationBox.textContent = newText;
-
-        // Force reflow pour que le fade-in soit bien déclenché
         void explicationBox.offsetWidth;
-
         explicationBox.classList.remove('fade-out');
     }, 0);
 }
@@ -24,4 +19,13 @@ document.querySelectorAll('.mot-cle').forEach((mot) => {
     mot.addEventListener('mouseleave', () => {
         changeTextWithFade("Survolez un mot-clé pour voir l'explication ici.");
     });
+});
+
+
+const encadre = document.querySelector('.encadre-haut');
+
+window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+    const targetTop = scrollY + 300; // 100px de marge depuis le haut
+    encadre.style.top = `${targetTop}px`;
 });
